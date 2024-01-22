72f034f8ff9eee35f4c16c16870a0649a85469d0
shenxiang <shenxiang@foursmile001.com>
2024-01-22 14:16:09 +0800

    Initial Commit

---
+++ ListWithImageHeader/ListWithImageHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListWithImageHeaderApp.swift
+//  ListWithImageHeader
+//
+//  Created by FS on 2024/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ListWithImageHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
