59dbd13ce9b116f9f65de33ad8023f58a259dd07
shenxiang <shenxiang@foursmile001.com>
2024-01-22 15:10:51 +0800

    完成效果

--- ListWithImageHeader/ContentView.swift
+++ ListWithImageHeader/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+let screenWidth = UIScreen.main.bounds.width
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                GeometryReader { proxy in
+                    let y = proxy.frame(in: .named("List")).minY
+                    
+                    VStack {
+                        Image(.p)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: screenWidth, height: y > 0 ? 340 + y : 340)
+                            .clipped()
+                            .ignoresSafeArea()
+                            .offset(y: -y)
+                    }
+                }
+                .frame(height: 340)
+                
+                VStack(spacing: 20) {
+                    
+                    ForEach(0...20, id: \.self) { _ in
+                        Color.gray
+                            .frame(height: 120)
+                            .clipShape(.rect(cornerRadius: 20))
+                    }
+                    
+                }
+                .padding(.all, 20)
+                .background(.white)
+            }
+            .coordinateSpace(.named("List"))
+            .navigationTitle("RARA")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
